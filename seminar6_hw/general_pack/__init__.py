da2bfc1decf31750c9909178255a6543ba39bbb4
EvgeniyLukashevich <humansyndr8me@gmail.com>
2023-06-17 18:44:18 +0800

    the 6th seminar homework added

--- seminar6_hw/general_pack/__init__.py
+++ seminar6_hw/general_pack/__init__.py
@@ -3,4 +3,5 @@ from .lesson_task4_5_6 import *
 from .lesson_task7 import *
 from .homework_task2_3 import *
 
-__all__ = ['guess_number', 'mystery', 'mysteries', 'is_valid_date']
+__all__ = ['guess_number', 'mystery', 'mysteries', 'is_valid_date',
+           'peaceful_queens', 'peaceful_queens_check', 'print_field']
